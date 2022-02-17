28892af84dcd722c2bf6d41751f814291e09d894
Larrire <linekerlarrire@gmail.com>
2022-02-17 07:36:34 -0300

    Creating Theme component and using into pages

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { Children } from 'react';
+import * as C from './styles';
+
+export const Header = () => {
+  return (
+    <C.Container>
+      <h1>Cadastro de desenvolvedor</h1>
+      <p>Faça seu cadastro na lista e receba um emprego instantaneamente!(Ou quase...)</p>
+    </C.Container>
+  )
+}
