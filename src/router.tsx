fe5d3aadf0b7aedb2329d6a3f9ffc3849495d196
Larrire <linekerlarrire@gmail.com>
2022-02-17 07:34:14 -0300

    Renaming pages folders

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route } from 'react-router-dom';
-import { FormStepOne } from './pages/FormStepOne'
-import { FormStepTwo } from './pages/FormStepTwo'
-import { FormStepThree } from './pages/FormStepThree'
+import { FormStepOne } from './pages/FormStep-1'
+import { FormStepTwo } from './pages/FormStep-2'
+import { FormStepThree } from './pages/FormStep-3'
 
 export const Router = () => {
   return (
