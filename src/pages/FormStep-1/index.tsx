7cc758b579bb75f11897df35aec0687a5fc8122d
Larrire <linekerlarrire@gmail.com>
2022-02-20 15:05:21 -0300

    Creatingg first step of the form

--- src/pages/FormStep-1/index.tsx
+++ src/pages/FormStep-1/index.tsx
@@ -1,11 +1,33 @@
 import * as C from './styles';
 import { Theme } from '../../components/Theme';
+import { useHistory } from 'react-router-dom'
 
 export const FormStepOne = () => {
+
+  const history = useHistory();
+
+  const handleNextStep = () => {
+    history.push('/step-two');
+  }
+
   return (
     <Theme>
       <C.Container>
-      <h1>Step one</h1>
+        <p>Passo 1/3</p>
+        <h1>Vamos começar com seu nome</h1>
+        <p>Preencha o campo abaixo com seu nome completo.</p>
+        
+        <hr />
+
+        <label htmlFor="">
+          Seu nome completo
+          <input 
+            type="text"
+            autoFocus
+          />
+        </label>
+
+        <button onClick={handleNextStep}>Próximo</button>
       </C.Container>
     </Theme>
   )
